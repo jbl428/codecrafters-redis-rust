c8b7533a6fcb8ef997df28227cc2177e5e199518
Jake Son <sonjeabin@gmail.com>
2024-04-28 18:48:05 +0900

    handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread::spawn;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -7,25 +8,27 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                loop {
-                    let mut buffer = [0; 512];
-                    match stream.read(&mut buffer) {
-                        Ok(n) => {
-                            if n == 0 {
-                                break;
-                            }
-                            let request = String::from_utf8_lossy(&buffer[..n]);
-                            println!("request: {}", request);
-                            if request.contains("PING") {
-                                let response = "+PONG\r\n";
-                                stream.write(response.as_bytes()).unwrap();
-                                stream.flush().unwrap();
-                            }
-                        }
-                        Err(e) => {
-                            println!("error: {}", e);
-                        }
-                    }
+                spawn(move || { handle_connection(&mut stream).unwrap(); });
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_connection(stream: &mut TcpStream) -> std::io::Result<()> {
+    loop {
+        let mut buffer = [0; 512];
+        match stream.read(&mut buffer) {
+            Ok(n) => {
+                if n == 0 {
+                    return Ok(());
+                }
+                let request = String::from_utf8_lossy(&buffer[..n]);
+                if request.contains("PING") {
+                    let response = "+PONG\r\n";
+                    stream.write_all(response.as_bytes())?;
                 }
             }
             Err(e) => {
