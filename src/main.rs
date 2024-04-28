2504bb5ca38e3a7dd924c8086e0c9dbebb3f8bcb
Jake Son <sonjeabin@gmail.com>
2024-04-28 12:50:01 +0900

    pass Response to PING

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -5,8 +6,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                // send PONG
+                let pong = "+PONG\r\n";
+                _stream.write(pong.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
