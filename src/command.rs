6941fce05d106580a7a015ed3d3e35a1a784087a
Jake Son <sonjeabin@gmail.com>
2024-05-12 08:44:01 +0900

    add test for command

--- src/command.rs
+++ src/command.rs
@@ -55,3 +55,38 @@ impl CommandDispatcher {
         None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ping() {
+        let dispatcher = CommandDispatcher::new();
+        let token = RespToken::BulkString("PinG".to_string());
+        let response = dispatcher.dispatch(&token);
+
+        assert_eq!(response, Some(RespToken::BulkString("PONG".to_string())));
+    }
+
+    #[test]
+    fn test_echo() {
+        let dispatcher = CommandDispatcher::new();
+        let token = RespToken::Array(vec![
+            RespToken::BulkString("ecHO".to_string()),
+            RespToken::BulkString("Hello".to_string()),
+        ]);
+        let response = dispatcher.dispatch(&token);
+
+        assert_eq!(response, Some(RespToken::BulkString("Hello".to_string())));
+    }
+
+    #[test]
+    fn test_unknown_command() {
+        let dispatcher = CommandDispatcher::new();
+        let token = RespToken::BulkString("unknown".to_string());
+        let response = dispatcher.dispatch(&token);
+
+        assert_eq!(response, None);
+    }
+}
